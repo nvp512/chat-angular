8572cb77f089afa81780a099d1e32a1e426decb0
phunv <nguyenvinhphu1111@gmail.com>
2018-01-05 15:21:26 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChatBox::Application.config.secret_key_base = 'f211b8049c1b4ae8a9eb4dd643c9fd1797a730bed142d3008bc4fc9132de4da4e3b2bb2f1a3b74901dec435ea7419d9c0329f38d4c9ea2f9a0d072e847f8b20b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChatBox::Application.config.session_store :cookie_store, key: '_chat-box_session'
